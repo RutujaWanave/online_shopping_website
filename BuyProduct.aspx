3d3093ff92cc9895016386ee1500c156f3474eba
Rutuja Anil Wanave <162110707+RutujaWanave@users.noreply.github.com>
2024-03-28 10:28:52 +0530

    Add files via upload

---
+++ BuyProduct.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home2.Master" AutoEventWireup="true" CodeBehind="BuyProduct.aspx.cs" Inherits="Project_online.BuyProduct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    </asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="GridView1" runat="server" Height="544px" Width="827px" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" HorizontalAlign="Center" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+        <Columns>
+            <asp:BoundField DataField="ProductID" HeaderText="Product ID" />
+            <asp:BoundField DataField="ProductName" HeaderText="Product Name" />
+            <asp:BoundField DataField="Prices" HeaderText="Prices" />
+            <asp:ImageField DataImageUrlField="ProductImage" HeaderText="Product Image">
+            </asp:ImageField>
+            <asp:CommandField ShowSelectButton="True" />
+        </Columns>
+    </asp:GridView>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   </br>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:Online_ShoppingConnectionString11 %>" SelectCommand="SELECT * FROM [Product_details] WHERE ([ProductID] = @ProductID)">
+        <SelectParameters>
+            <asp:QueryStringParameter Name="ProductID" QueryStringField="ProductID" Type="Int32" DefaultValue="1" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+    </asp:Content>
---
+++ BuyProduct.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Project_online
+{
+    public partial class BuyProduct : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //    if (!IsPostBack)
+            //    {
+            //        // Session["buyitems"] = "false";
+            //        DataTable dt = new DataTable();
+            //        DataRow dr;
+
+
+
+            //        dt.Columns.Add("ProductID");
+            //        dt.Columns.Add("ProductName");
+            //        dt.Columns.Add("Prices");
+            //        dt.Columns.Add("ProductImage");
+
+            //        if (Request.QueryString["ProductID"] != null)
+            //        {
+            //            if (Session["buyitems"] == null)
+            //            {
+
+            //                dr = dt.NewRow();
+            //                string connection1 = "Data Source=DESKTOP-R5569UO\\SQLEXPRESS; Initial Catalog=Online_Shopping; Integrated Security=true";
+            //                SqlConnection scon = new SqlConnection(connection1);
+
+            //                string Query = "select * from Product_details where ProductID=" + Request.QueryString["id"];
+            //                SqlCommand cmd = new SqlCommand();
+            //                //  SqlDataAdapter da = new SqlDataAdapter("select * from Product_details where ProductID=" + Request.QueryString["id"],connection);
+            //                cmd.CommandText = Query;
+            //                cmd.Connection = scon;
+            //                SqlDataAdapter da = new SqlDataAdapter();
+            //                da.SelectCommand = cmd;
+            //                DataSet ds = new DataSet();
+
+            //                da.Fill(ds);
+
+
+
+            //                dr["ProductID"] = dt.Rows[0]["ProductID"].ToString();
+            //                dr["ProductName"] = dt.Rows[0]["ProductName"].ToString();
+            //                dr["ProductImage"] = dt.Rows[0]["ProductImage"].ToString();
+            //                dr["Prices"] = dt.Rows[0]["Prices"].ToString();
+
+
+            //                dt.Rows.Add(dr);
+            //                GridView1.DataSource = dt;
+            //                GridView1.DataBind();
+            //                Session["buyitems"] = dt;
+            //                Response.Redirect("BuyProduct.aspx");
+
+
+
+            //            }
+            //            else
+            //            {
+
+            //                if (Session["buyitems"] == null)
+            //                {
+            //                    // dt = (DataTable)Session["byitmes"];
+            //                    int sr;
+            //                    sr = dt.Rows.Count;
+
+            //                    dr = dt.NewRow();
+            //                    dt.Rows.Add(dr);
+            //                    String mycon = "Data Source=DESKTOP-R5569UO\\SQLEXPRESS; Initial Catalog=Online_Shopping; Integrated Security=true";
+            //                    SqlConnection scon = new SqlConnection(mycon);
+            //                    string Query = "select * from Product_details where ProductID=" + Request.QueryString["id"];
+            //                    SqlCommand cmd = new SqlCommand();
+
+            //                    cmd.CommandText = Query;
+            //                    cmd.Connection = scon;
+            //                    SqlDataAdapter da = new SqlDataAdapter();
+            //                    da.SelectCommand = cmd;
+            //                    DataSet ds = new DataSet();
+            //                    da.Fill(ds);
+
+            //                    dr["ProductID"] = ds.Tables[0].Rows[0]["ProductID"].ToString();
+            //                    dr["ProductName"] = ds.Tables[0].Rows[0]["ProductName"].ToString();
+            //                    dr["ProductImage"] = ds.Tables[0].Rows[0]["ProductImage"].ToString();
+            //                    dr["Prices"] = ds.Tables[0].Rows[0]["Prices"].ToString();
+            //                    dt.Rows.Add(dr);
+            //                    GridView1.DataSource = dt;
+            //                    GridView1.DataBind();
+            //                    Session["buyitems"] = dt;
+            //                    Response.Redirect("BuyProduct.aspx");
+            //                }
+            //            }
+            //        }
+            //        else
+            //        {
+            //            dt = (DataTable)Session["buyitmes"];
+            //            GridView1.DataSource = dt;
+            //            GridView1.DataBind();
+            //        }
+
+
+            //    }
+
+            //}
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow gr = GridView1.SelectedRow;
+            Response.Redirect("BUY_NOW.aspx?ProductID=" + gr.Cells[0].Text);// "&ProductName="+gr.Cells[1].Text+ "Prices="+gr.Cells[2].Text);
+        }
+
+        protected void btn_BUY_Click(object sender, EventArgs e)
+        {
+            //GridViewRow gr = GridView1.SelectedRow;
+            //Response.Redirect("BUYNOW.aspx?Product ID=" + gr.Cells[0].Text + "Product Name=" + gr.Cells[1].Text + "Prices=" + gr.Cells[2].Text);
+        }
+    }
+}
